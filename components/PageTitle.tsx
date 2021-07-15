e9da4d07bad09cbd43c29401eb4a4c314d823b13
Alex Serban <al.se@me.com>
2021-07-15 12:27:56 -0400

    Add page titles

---
+++ components/PageTitle.tsx
@@ -0,0 +1,18 @@
+import {TEST_NET} from "lib/constants"
+import publicConfig from "lib/publicConfig"
+import Head from "next/head"
+
+export const BASE_HTML_TITLE = `Flow ${
+  publicConfig.network === TEST_NET ? "Testnet" : "Canarynet"
+} Faucet`
+
+export default function PageTitle({children}: {children?: string}) {
+  const title = [children, BASE_HTML_TITLE].filter(Boolean).join(" - ")
+
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta property="og:title" content={title} key="title" />
+    </Head>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-/** @jsxImportSource theme-ui */
 import AppContainer from "components/AppContainer"
 import CreateAccountPanel from "components/CreateAccountPanel"
 import Header from "components/Header"
+import PageTitle from "components/PageTitle"
 
 export default function Home() {
   return (
     <AppContainer>
+      <PageTitle>Create Account</PageTitle>
       <Header fund={true} />
       <CreateAccountPanel />
     </AppContainer>
