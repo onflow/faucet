a8833f44e86deab578c8c7829d8b745063e6cd53
Mackenzie <remember.mackenzie@gmail.com>
2021-07-15 11:53:59 -0700

    Update FundAccountSubmitted.tsx

--- components/FundAccountSubmitted.tsx
+++ components/FundAccountSubmitted.tsx
@@ -66,7 +66,7 @@ export default function FundAccountSubmitted({
                   variant="secondary"
                   sx={{fontSize: 1}}
                 >
-                  View Wallet
+                  View Account
                 </Link>
               </Flex>
             </Box>
