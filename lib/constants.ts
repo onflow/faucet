e7a24ea484da95ec31e3cfd37ca09691ea1955c6
Alex Serban <al.se@me.com>
2021-09-16 09:09:14 -0400

    Add Tanooki Labs footer link

--- lib/constants.ts
+++ lib/constants.ts
@@ -12,7 +12,7 @@ export const ACCOUNTS_KEYS_DOCS_URL =
   "https://docs.onflow.org/concepts/accounts-and-keys/"
 export const H_CAPTCHA_URL = "https://www.hcaptcha.com/"
 export const FAUCET_GITHUB_URL = "https://www.github.com/onflow/faucet/issues"
-export const DEVELOPER_DOCS_URL = "https://docs.onflow.org"
+export const TANOOKI_LABS_URL = "https://tanookilabs.com/"
 export const DISCORD_URL = "https://discord.gg/flow"
 
 export const PUBLIC_KEY_FORMAT_ERROR =
